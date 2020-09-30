5aaf549a1e4bc1d77a8ea66f83bbf402ba5241c1
kristiandrex <cristiantorres.87@hotmail.com>
2020-09-30 13:24:35 -0500

    Fix route to serve static files

--- src/index.ts
+++ src/index.ts
@@ -17,9 +17,9 @@ app.use(cors({ origin: '/', allowedHeaders: 'Authorization' }));
 app.use('/api', routes);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve(__dirname, '../client/build')));
+  app.use(express.static(path.resolve(__dirname, '../client/build/')));
 
-  app.get('/*', (_req, res) => {
+  app.get('*', (_req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
   });
 }
