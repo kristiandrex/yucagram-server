c4aafcd6c8855adbda6ab2277bdb8e6d13b9d9c9
kristiandrex <cristiantorres.87@hotmail.com>
2020-09-29 23:20:50 -0500

    Changes for deploy

--- src/index.ts
+++ src/index.ts
@@ -14,14 +14,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({ origin: '/', allowedHeaders: 'Authorization' }));
-app.use(express.static('public'));
 app.use('/api', routes);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build.html'));
+  app.get('/*', (_req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
   });
 }
 
